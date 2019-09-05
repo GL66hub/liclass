706af92620e526975448eb44ab2f57a7601cb7b8
GL66hub <2780191189@qq.com>
2019-09-05 13:13:26 +0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+Author:GL66hub
+Date:2019-9-5 
+Program:œ‘ æ"Hello World"
+*/
+public class HelloWorld{
+	public static void main(String[] args){
+      		System.out.println("Hello World");	
+  	}
+}
