763aa665c30405489ac8b9d2a8b45a1d52137c81
GL66hub <54879865+GL66hub@users.noreply.github.com>
2019-11-17 20:14:48 +0800

    Delete Manager.java

--- 第六章2/Manager.java
+++
@@ -1,9 +0,0 @@
-package com.apple.chaptersix;
-
-public class Manager {
-    public static void main(String[] args){
-        BookServiceImpl bookService = new BookServiceImpl();
-        bookService.makebooks();
-        bookService.showbooks();
-    }
-}
